dab5938f235aaacafce6e4cd6bdb33ef7c93a7c0
Austin Pocus <ajpocus@gmail.com>
2013-08-02 22:04:29 -0400

    first version of timestamp generation

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 exports = module.exports = function concreteTimestamps(schema, options) {
-
+  schema.pre('save', function (next) {
+    if (this.isNew) {
+      this.createdAt= new Date();
+    } else {
+      this.updatedAt = new Date();
+    }
+    next();
+  });
 };
